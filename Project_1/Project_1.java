58dd89b03a76adeb483fd8dbd86c5cbe2a04f401
SC0FI <Maddoxjacob64@proton.me>
2025-01-24 08:00:04 -0600

    Project 1

---
+++ Project_1/Project_1.java
@@ -0,0 +1,26 @@
+package Project_1;
+import java.util.Scanner;
+
+public class Project_1 {
+    public static void main (String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your grade");
+        double score = scanner.nextDouble();
+        if (score >= 60) {
+            System.out.println("D");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+        else if (score >= 80) {
+            System.out.println("B");
+        }
+        else if (score >= 90) {
+            System.out.println("A");
+        }
+        else if (score < 60) {
+            System.out.println("F");
+        }
+    }
+
+}
---
+++ out/production/CMPS-2800/README.md
@@ -0,0 +1,1 @@
+# A collection of all of my projects from my CMPS-2800 class at SELU
