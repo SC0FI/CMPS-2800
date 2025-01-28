c18950b54695a70f8820fcb5cc51ae32e94371be
SC0FI - Southeastern <maddoxjacob64@proton.me>
2025-01-28 12:10:22 -0600

    1/28/25 attempt 1

---
+++ ClassJan28th2025/loopsReview.java
@@ -0,0 +1,23 @@
+package ClassJan28th2025;
+import java.util.Scanner;
+
+public class loopsReview {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        double grade = 0;
+        double sum = 0;
+        double count = 0;
+        while(grade >= 0) {
+            System.out.println("please input grade from 0 to 110 (-1 to terminate)");
+            grade = input.nextDouble();
+            if (grade > 110) {
+                System.out.println("Invalid");
+            }
+            else {
+                sum += grade;
+                count++;
+                System.out.println("sum is " + sum);
+            }
+        }
+    }
+}
