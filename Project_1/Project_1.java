1ce25498117e9877166b9892436fbf3f4e479051
SC0FI - Laptop <maddoxjacob64@proton.me>
2025-01-29 15:58:55 -0600

    fixing incorrect hierarchy

--- Project_1/Project_1.java
+++ Project_1/Project_1.java
@@ -6,17 +6,17 @@ public class Project_1 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your grade");
         double score = scanner.nextDouble();
-        if (score >= 60) {
-            System.out.println("D");
-        }
-        else if (score >= 70) {
-            System.out.println("C");
+        if (score >= 90) {
+            System.out.println("A");
         }
         else if (score >= 80) {
             System.out.println("B");
         }
-        else if (score >= 90) {
-            System.out.println("A");
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+        else if (score >= 60) {
+            System.out.println("D");
         }
         else if (score < 60) {
             System.out.println("F");
